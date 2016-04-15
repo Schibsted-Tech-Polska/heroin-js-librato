f5b1eeced7575f00f0bbf07c680ce46f8d04db2c
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-04-15 13:37:56 +0200

    basic import and export work

--- tutorial/librato.js
+++ tutorial/librato.js
@@ -1,3 +1,48 @@
 const libratoConfigurator = require('../index')(process.env.USERNAME, process.env.PASSWORD);
 
-libratoConfigurator.fetchAllAlerts().then((result) => console.log(JSON.stringify(result, null, 2)))
+//libratoConfigurator.export().then((result) => console.log(result))
+
+//libratoConfigurator.deleteAllAlerts().then((result) => console.log(result));
+
+libratoConfigurator.createOrUpdate({
+  "alerts": [
+    {
+      "name": "myapp.test.alert4",
+      "description": null,
+      "conditions": [
+        {
+          "type": "above",
+          "metric_name": "router.status.5xx",
+          "source": null,
+          "threshold": 1000,
+          "duration": 2000
+        }
+      ],
+      "services": [],
+      "attributes": null,
+      "active": true,
+      "version": 2,
+      "rearm_seconds": 120,
+      "rearm_per_signal": false
+    },
+    {
+      "name": "myapp.test.alert3",
+      "description": null,
+      "conditions": [
+        {
+          "type": "above",
+          "metric_name": "router.status.5xx",
+          "source": null,
+          "threshold": 2000,
+          "duration": 2000
+        }
+      ],
+      "services": [],
+      "attributes": {},
+      "active": true,
+      "version": 2,
+      "rearm_seconds": 120,
+      "rearm_per_signal": false
+    }
+  ]
+}).then((result) => console.log(result)).catch((err) => console.error(err.stack));
