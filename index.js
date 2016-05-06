22ea94fab0396439ed503c7483683d15262cd081
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-05-06 16:44:01 +0200

    use token instead of password

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = require('./lib/app')
 const request = require('good-guy-http')({cache: false})
 
-function client(username, password) {
-  const librato = require('./lib/libratoClient')(username, password, request)
+function client(username, token) {
+  const librato = require('./lib/libratoClient')(username, token, request)
   return app(librato)
 }
 
@@ -10,11 +10,11 @@ var plugin = {
   librato: {
     alerts: {
       configure: function (config, configVars) {
-        var librato = client(configVars.LIBRATO_USER, configVars.LIBRATO_PASSWORD)
+        var librato = client(configVars.LIBRATO_USER, configVars.LIBRATO_TOKEN)
         return librato.createOrUpdate(config)
       },
       export: function (configVars) {
-        var librato = client(configVars.LIBRATO_USER, configVars.LIBRATO_PASSWORD)
+        var librato = client(configVars.LIBRATO_USER, configVars.LIBRATO_TOKEN)
         return librato.export()
       }
     }
