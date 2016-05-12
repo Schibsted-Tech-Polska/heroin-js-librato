8bba5ce1448c99db48cb008ac6bfeb5e0863217b
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-05-12 16:18:53 +0200

    started adding tests around alerts configuration

--- test/appTest.js
+++ test/appTest.js
@@ -1,23 +1,37 @@
 const test = require('tape')
 const alertsConfig = [ {
-  name: 'myapp.test.alert',
+  name: 'myapp.test.alert1',
   services: [ {
     type: 'slack',
     settings: {url: 'https://hooks.slack.com/services/xyz'},
     title: 'Slack notification'
   } ]
 } ]
+const updatedAlertsConfig = [
+  {
+    name: 'myapp.test.alert1',
+    newField: 'update trigger',
+    services: [ {
+      type: 'slack',
+      settings: {url: 'https://hooks.slack.com/services/xyz'},
+      title: 'Slack notification'
+    } ]
+  },
+  {
+    name: 'myapp.test.alert2'
+  }
+]
 const librato = {
   fetchAllAlerts () {
     return Promise.resolve({body: JSON.stringify({alerts: alertsConfig})})
   },
-  createAlerts () {
-    return []
+  createAlerts (alerts) {
+    return alerts.map((alert) => Promise.resolve())
   },
-  updateAlerts () {
-    return []
+  updateAlerts (alerts) {
+    return alerts.map((alert) => Promise.resolve())
   },
-  deleteAlerts () {
+  deleteAlerts (alerts) {
     return []
   }
 
@@ -29,5 +43,13 @@ test('should ignore alert update when no changes', (t) => {
 
   app.createOrUpdate(alertsConfig).then(function (message) {
     t.equal(message, 'modified 0 alerts')
+  }).catch(t.fail)
+})
+
+test('should trigger 2 modifications when one alerts created and one updated', (t) => {
+  t.plan(1)
+
+  app.createOrUpdate(updatedAlertsConfig).then(function (message) {
+    t.equal(message, 'modified 2 alerts')
   }).catch(t.fail)
 })
