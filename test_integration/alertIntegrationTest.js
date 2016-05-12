277787c3efc88f59c92c2ffaca89d5f264a774eb
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-05-12 13:50:24 +0200

    breaking change - stop passing alerts key to librato

--- test_integration/alertIntegrationTest.js
+++ test_integration/alertIntegrationTest.js
@@ -23,97 +23,91 @@ test('should delete all alerts', (t) => {
   t.plan(1)
 
   configurator.deleteAllAlerts()
-      .then(() => configurator.fetchAllAlerts())
-      .then((result) => {
-        t.equal(result.length, 0)
+    .then(() => configurator.fetchAllAlerts())
+    .then((result) => {
+      t.equal(result.length, 0)
+    })
+    .catch((err) => {
+      console.error(err)
+      t.fail(err)
+    })
+})
+
+withEmptyAlerts(
+  test('should create a new alert', (t) => {
+    t.plan(1)
+
+    configurator.createOrUpdate(
+      [ {
+        'name': 'myapp.status5xx.high',
+        'rearm_seconds': 108000,
+        'conditions': [ {
+          'type': 'above',
+          'metric_name': 'router.status.5xx',
+          'summary_function': 'sum',
+          'threshold': 40,
+          'duration': 600
+        } ],
+        'version': 2
+      } ]
+    ).then((result) => t.equal(result, 'modified 1 alerts'))
+      .catch((err) => {
+        console.error(err.stack)
+        t.fail(err)
       })
+  })
+)
+
+withEmptyAlerts(
+  test('should delete an alert', (t) => {
+    t.plan(1)
+
+    configurator.createOrUpdate(
+      [ alertConfig('myapp.test.alert') ]
+    )
+      .then(() => {
+        return configurator.createOrUpdate([])
+      })
+      .then((result) => {
+        t.equal(result, 'modified 1 alerts')
+      })
+      .catch((err) => {
+        console.error(err.stack)
+        t.fail(err)
+      })
+  })
+)
+
+withEmptyAlerts(
+  test('should batch create alerts', (t) => {
+    t.plan(1)
+
+    configurator.createOrUpdate([ alertConfig('myapp.test.alert1'),
+        alertConfig('myapp.test.alert2') ]
+    ).then((result) => t.equal(result, 'modified 2 alerts'))
       .catch((err) => {
         console.error(err)
         t.fail(err)
       })
-})
-
-withEmptyAlerts(
-    test('should create a new alert', (t) => {
-      t.plan(1)
-
-      configurator.createOrUpdate({
-        alerts: [ {
-          'name': 'myapp.status5xx.high',
-          'rearm_seconds': 108000,
-          'conditions': [ {
-            'type': 'above',
-            'metric_name': 'router.status.5xx',
-            'summary_function': 'sum',
-            'threshold': 40,
-            'duration': 600
-          } ],
-          'version': 2
-        } ]
-      }).then((result) => t.equal(result, 'modified 1 alerts'))
-          .catch((err) => {
-            console.error(err.stack)
-            t.fail(err)
-          })
-    })
+  })
 )
 
 withEmptyAlerts(
-    test('should delete an alert', (t) => {
-      t.plan(1)
+  test('should batch update alerts', (t) => {
+    t.plan(1)
 
-      configurator.createOrUpdate({
-        alerts: [ alertConfig('myapp.test.alert') ]
-      }).then(() => {
-        return configurator.createOrUpdate({alerts: []})
-      }).then((result) => {
-        t.equal(result, 'modified 1 alerts')
+    configurator.createOrUpdate([ alertConfig('myapp.test.alert3'),
+        alertConfig('myapp.test.alert4'),
+        alertConfig('myapp.test.alert5') ]
+    ).then(() => configurator.createOrUpdate(
+        [ alertConfig('myapp.test.alert3', 2000),
+          alertConfig('myapp.test.alert4') ]))
+      .then((result) => t.equal(result, 'modified 2 alerts'))
+      .catch((err) => {
+        console.error(err)
+        t.fail(err)
       })
-          .catch((err) => {
-            console.error(err.stack)
-            t.fail(err)
-          })
-    })
-)
-
-withEmptyAlerts(
-    test('should batch create alerts', (t) => {
-      t.plan(1)
-
-      configurator.createOrUpdate({
-        alerts: [
-          alertConfig('myapp.test.alert1'),
-          alertConfig('myapp.test.alert2')
-        ]
-      }).then((result) => t.equal(result, 'modified 2 alerts'))
-          .catch((err) => {
-            console.error(err)
-            t.fail(err)
-          })
-    })
-)
-
-withEmptyAlerts(
-    test('should batch update alerts', (t) => {
-      t.plan(1)
-
-      configurator.createOrUpdate({
-        alerts: [
-          alertConfig('myapp.test.alert3'),
-          alertConfig('myapp.test.alert4'),
-          alertConfig('myapp.test.alert5')
-        ]
-      }).then(() => configurator.createOrUpdate({
-        alerts: [
-          alertConfig('myapp.test.alert3', 2000),
-          alertConfig('myapp.test.alert4')
-        ]
-      })).then((result) => t.equal(result, 'modified 2 alerts'))
-          .catch((err) => {
-            console.error(err)
-            t.fail(err)
-          })
-    })
+  })
 )
 
 test('should export all alerts', (t) => {
